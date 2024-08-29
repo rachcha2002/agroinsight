aecc896605728387b2fdc367b2ceeff1ad1c84ed
kavinda0126 <127762257+kavinda0126@users.noreply.github.com>
2024-08-29 21:49:07 +0530

    minor changes in navigation web app(disease)

--- agroinsightadmin/src/components/AgriAdmin/DiseaseTracker/DiseaseTracker.js
+++ agroinsightadmin/src/components/AgriAdmin/DiseaseTracker/DiseaseTracker.js
@@ -2,10 +2,10 @@ import React from "react";
 import PageTitle from "../AgriPageTitle";
 import "../Main/Main.css";
 import { Tabs, Tab } from "react-bootstrap";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import PestManagement from "./Pages/PestManagement";
 import AITool from "./Pages/AITool";
-import DiseaseAlerts from "./Pages/DiseaseAlerts";  // Import the new component
+import DiseaseAlerts from "./Pages/DiseaseAlerts"; // Import the new component
 
 export default function DiseaseTracker() {
   return (
@@ -15,15 +15,19 @@ export default function DiseaseTracker() {
         url="/agriadmin/diseases"
       />
       <br />
-      <Tabs defaultActiveKey="pest-management" id="disease-tracker-tabs" className="mb-3">
+      <Tabs
+        defaultActiveKey="disease-alerts"
+        id="disease-tracker-tabs"
+        className="mb-3"
+      >
+        <Tab eventKey="disease-alerts" title="Disease Alerts">
+          <DiseaseAlerts />
+        </Tab>
         <Tab eventKey="pest-management" title="Pest and Disease Complaints">
           <PestManagement />
         </Tab>
         <Tab eventKey="ai-tool" title="AI Tool">
           <AITool />
-        </Tab>
-        <Tab eventKey="disease-alerts" title="Disease Alerts">  
-          <DiseaseAlerts />
         </Tab>
       </Tabs>
     </main>
