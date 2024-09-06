dfce12ac800d6f468800ce08bebe647af9dc5b97
M.A.Rachith Tharana <rachcha2002@gmail.com>
2024-09-06 19:03:55 +0530

    routing updated from super admin

--- agroinsightadmin/src/components/MarketAdmin/MASidebar/SideBar.js
+++ agroinsightadmin/src/components/MarketAdmin/MASidebar/SideBar.js
@@ -2,8 +2,13 @@ import React from "react";
 import "./SideBar.css";
 import navList from "../../../data/MarketAdmin/navItem";
 import NavItem from "./NavItem";
+import { BsArrowLeft } from "react-icons/bs";
+import { Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
 function SideBar() {
+  // To redirect after success
+  const navigate = useNavigate();
   return (
     <aside id="sidebar" className="sidebar">
       <ul className="sidebar-nav" id="sidebar-nav">
@@ -11,6 +16,13 @@ function SideBar() {
           <NavItem key={nav._id} nav={nav} />
         ))}
       </ul>
+      <Button
+        variant="dark"
+        onClick={() => navigate("/")}
+        style={{ margin: "10px" }}
+      >
+        <BsArrowLeft /> Back
+      </Button>
     </aside>
   );
 }
--- agroinsightadmin/src/data/navItem.js
+++ agroinsightadmin/src/data/navItem.js
@@ -6,33 +6,15 @@ const navList = [
   },
   {
     _id: 2,
-    name: "Documents",
+    name: "Agriclutural Management",
     icon: "bi bi-menu-button-wide",
-    children: [
-      { _id: 21, name: "Customers", icon: "bi bi-circle" },
-      { _id: 22, name: "Suppliers", icon: "bi bi-circle" },
-      { _id: 23, name: "Logistic", icon: "bi bi-circle" },
-    ],
+    href: "/agriadmin/",
   },
   {
     _id: 3,
-    name: "Forms",
+    name: "Market Management",
     icon: "bi bi-journal-text",
-  },
-  {
-    _id: 4,
-    name: "Tables",
-    icon: "bi bi-layout-text-window-reverse",
-  },
-  {
-    _id: 5,
-    name: "Charts",
-    icon: "bi bi-bar-chart",
-  },
-  {
-    _id: 6,
-    name: "Icons",
-    icon: "bi bi-gem",
+    href: "/market/",
   },
 ];
 
